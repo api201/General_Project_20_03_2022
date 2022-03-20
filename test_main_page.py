bb76aadc0f92b170f389e624950e00b5c8cae113
api201 <api201ok10@gmail.com>
2022-03-20 22:51:56 +0300

    addition info to test_main + new file example1.py

--- test_main_page.py
+++ test_main_page.py
@@ -5,5 +5,6 @@ def test_guest_can_go_to_login_page(browser):
     browser.get(link)
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+    print('\nFirst test is PASSED!')
 
 # pytest -v --tb=line --language=en -m need_review .\test_main_page.py
