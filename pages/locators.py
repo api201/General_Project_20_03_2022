8ff9f03f4fba5549359084a39cafd926166b708c
api201 <api201ok10@gmail.com>
2022-03-22 11:43:34 +0300

    Update 22-03-2022: new locators ans *MainPageLocators.LOGIN_LINK + broken click()

---
+++ pages/locators.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    #old LOGIN_LINK = (By.ID, "registration_link")
+    #LOGIN_LINK = (By.ID, "login_link")
+    #LOGIN_LINK = (By.XPATH, "//*[@class='icon-signin']")
+    #LOGIN_LINK = (By.XPATH, "//*[@href='/en-gb/accounts/login/']")
+
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,14 +1,23 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+        #OLD2 login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK), "#login_link"
+        #login_link.click()
+        #LOGIN_LINK.click()
 
-    # def should_be_login_link(self):
-    #     self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+
+    #1 def should_be_login_link(self):
+    #1     self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
 
     def should_be_login_link(self):
-        #assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+        #OLD1 assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
+        #OLD2 assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented" # "*" is a tuple?
+
+
+        #assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+        #2 changes
