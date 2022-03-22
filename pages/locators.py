a540a0e7ea7bae9b6f579f42f48da51323110097
api201 <api201ok10@gmail.com>
2022-03-23 01:54:28 +0300

    Update 23-03-2022: login_page and locators

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,10 @@ class MainPageLocators():
     #LOGIN_LINK = (By.XPATH, "//*[@class='icon-signin']")
     #LOGIN_LINK = (By.XPATH, "//*[@href='/en-gb/accounts/login/']")
 
+class LoginPageLocators:
+    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
+    REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+    REGISTER_EMAIL = (By.CSS_SELECTOR, '#id_registration-email')
+    REGISTER_PASSWORD1 = (By.CSS_SELECTOR, '#id_registration-password1')
+    REGISTER_PASSWORD2 = (By.CSS_SELECTOR, '#id_registration-password2')
+    REGISTER_SUBMIT = (By.CSS_SELECTOR, 'button[name="registration_submit"]')
